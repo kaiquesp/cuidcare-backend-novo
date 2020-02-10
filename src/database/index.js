8319984189022b232daa86833515369e4f6d5e8f
Kaique Alves <kaiqueexp@gmail.com>
2020-02-10 14:36:41 -0300

    Ajuste

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/noderest', { useMongoClient: true });
+mongoose.connect('mongodb://cuidcare:cuidcare2019@ds011912.mlab.com:11912/heroku_s2nwmkk0', { useMongoClient: true });
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
