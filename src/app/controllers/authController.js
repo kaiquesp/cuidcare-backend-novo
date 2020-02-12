41e0770ecef852b7a75d7efc2ac1e82df7df65fe
Kaique Alves <kaiqueexp@gmail.com>
2020-02-12 10:22:52 -0300

    envio de email ao registrar usuário

--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -45,8 +45,34 @@ router.post('/register', async (req, res) => {
     user.password = undefined;
 
 
-    return res.send({
-      user
+    const transporter = nodemailer.createTransport({
+      host: "email-ssl.com.br",
+      port: 465,
+      secure: true, // true for 465, false for other ports
+      auth: {
+        user: "contato@kaique.provisorio.ws",
+        pass: "Tecno1101943.."
+      },
+      tls: { rejectUnauthorized: false }
+    });
+
+    const mailOptions = {
+      from: 'contato@kaique.provisorio.ws',
+      to: 'kaiqueexp@gmail.com',
+      subject: 'E-mail enviado usando Node!',
+      html : { path: './src/resources/mail/auth/forgot_password.html' }
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email enviado: ' + info.response);
+        return res.status(200).send({
+          message: 'E-mail enviado com sucesso',
+          user: user
+        })
+      }
     });
 
   } catch (err) {
